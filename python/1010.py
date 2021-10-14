0a79cbd96c0a3fb3487952f5267c64fef4df65cb
Lailson Henrique <lailson.pi@gmail.com>
2021-10-14 15:56:45 -0300

    1010 - calculo simples

---
+++ python/1010.py
@@ -0,0 +1,6 @@
+codigo1, numero1, valor1 = map(float, input().split(" "))
+codigo2, numero2, valor2 = map(float, input().split(" "))
+
+total = numero1 * valor1 + numero2 * valor2
+ 
+print(f'VALOR A PAGAR: R$ {total:.2f}') 
