e6357f941d5736ddfac25579f8c3c5bc911686e3
Lailson Henrique <lailson.pi@gmail.com>
2021-10-16 12:27:22 -0300

    Descrição das questões

--- python/1021.py
+++ python/1021.py
@@ -1,5 +1,5 @@
+from decimal import *
 valor = float(input())
-
 notas = [100.00, 50.00, 20.00, 10.00, 5.00, 2.00]
 resto = valor
 
@@ -7,11 +7,13 @@ print(f'NOTAS:')
 for n in notas:
   quantidade = resto // n
   resto = resto % n
-  print(f'{quantidade} nota(s) de RS {n}')
+  print(f'{quantidade} nota(s) de R$ {n:.2f}')
+  print(f'{resto}')
 
 print(f'MOEDAS:')
 moedas = [1.00, 0.50, 0.25, 0.10, 0.05, 0.01]
 for m in moedas:
   quantidade = resto // m
-  resto = resto % m
-  print(f'{quantidade} moeda(s) de R$ {m}')
+  resto = resto - (quantidade * m)
+  print(f'{quantidade} moeda(s) de R$ {m:.2f}')
+  print(f'{resto}')
